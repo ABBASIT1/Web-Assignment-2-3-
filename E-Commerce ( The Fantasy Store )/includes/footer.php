c8e273d264a9d910b6620f53530e7e29e4a218e5
ABBASIT1 <44797913+ABBASIT1@users.noreply.github.com>
2019-04-29 07:17:08 +0500

    Commit By Abdul Basit
    Updated files

---
+++ E-Commerce ( The Fantasy Store )/includes/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="container">
+        <center>
+            <p>Copyright &copy; The Fantasy Store. All Rights Reserved  |  Contact Us: +92 323 5234601</p>	
+        </center>
+    </div>
+</footer>
